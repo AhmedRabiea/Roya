d5ebe9884adab97d325341385291590788437ebc
ahmedrabie <a.rabie.eissa@gmail.com>
2024-01-14 00:04:25 +0200

    footer updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,55 +1,29 @@
 import Link from "next/link";
 import React from "react";
 import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
     <div className="flex flex-col bg-bunker text-white py-4 mt-36">
-      <div className="container mx-auto border-b-2 pb-2">
-        <div className="flex items-center justify-between gap-5">
-          <Link href="/">
-            <img
-              src="../assets/imgs/logo.svg"
-              alt="Roya-Logo"
-              className="w-36"
-            />
+      <div className="container mx-auto border-b-2 flex flex-col gap-5">
+        <div className="grid grid-cols-3 items-center pb-7">
+          <div className="col-span-1 flex flex-col gap-2">
+            <Link href="/">
+              <img
+                src="../assets/imgs/logo.svg"
+                alt="Roya-Logo"
+                className="w-36"
+              />
+            </Link>
             <p>
               A new way to organize and develop
               <br /> your business
             </p>
-          </Link>
-          <ul className="text-center">
-            <h1 className="text-base underline font-bold">Usefuel Links</h1>
-            <Link href="/">
-              <li>Home</li>
-            </Link>
-            <Link href="/">
-              <li>About Us</li>
-            </Link>
-            <Link href="/">
-              <li>Features</li>
-            </Link>
-            <Link href="/">
-              <li>Solutions</li>
-            </Link>
-          </ul>
-          <ul className="text-center">
-            <h1 className="font-bold text-base underline">Community</h1>
-            <Link href="/">
-              <li>Home</li>
-            </Link>
-            <Link href="/">
-              <li>About Us</li>
-            </Link>
-            <Link href="/">
-              <li>Features</li>
-            </Link>
-            <Link href="/">
-              <li>Solutions</li>
-            </Link>
-          </ul>
-          <ul className="text-center">
-            <h1 className="font-bold text-base underline">Partner</h1>
+          </div>
+          <ul className="">
+            <h1 className="underline">Partner</h1>
             <Link href="/">
               <li>Our Parnter</li>
             </Link>
@@ -57,19 +31,34 @@ const Footer = () => {
               <li>Become One Of Our Partners</li>
             </Link>
           </ul>
-        </div>
-      </div>
-
-      <div className="container mx-auto pt-4">
-        <div className="flex justify-between">
-          <p>Ro&apos;ya© All rights reserved Copyrights 2024</p>
-          <div className="flex gap-4 items-center text-xl">
-            <FaFacebook />
-            <FaLinkedin />
-            <FaGithub />
-            <FaTwitter />
+          <div className="flex flex-col gap-3">
+            <h1>Ro&apos;ya News Letter</h1>
+            <div className="flex gap-2">
+              <Input placeholder="Enter Your Email" />
+              <Button variant="getStarted">Subscribe</Button>
+            </div>
+            <p className="text-gray-300">
+              By Subscribing, you agree to our Privacy Policy
+            </p>
+            <div className="flex gap-4 items-center text-2xl">
+              <div className="border-2 rounded-lg border-white/90 p-2">
+                <FaFacebook />
+              </div>
+              <div className="border-2 rounded-lg border-white/90 p-2">
+                <FaLinkedin />
+              </div>
+              <div className="border-2 rounded-lg border-white/90 p-2">
+                <FaGithub />
+              </div>
+              <div className="border-2 rounded-lg border-white/90 p-2">
+                <FaTwitter />
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="container mx-auto pt-4 text-center">
+        <p>Ro&apos;ya© All rights reserved Copyrights 2024</p>
       </div>
     </div>
   );
